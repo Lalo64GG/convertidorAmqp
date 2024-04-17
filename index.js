3f5b076f49fe8f937456203501ca2ea2c4d07080
Lalo64GG <223257@ids.upchiapas.edu.mx>
2024-04-17 14:41:45 -0600

    Refactor code to include distance measurements and calculate percentage in index.js

--- index.js
+++ index.js
@@ -13,17 +13,39 @@ let datosESP32 = [];
 app.post('/esp32', (req, res) => {
   const temperatura = req.body.temperatura;
   const humedad = req.body.humedad;
-  const nivel_luz = req.body.nivel_luz;
+  const distancia_1 = req.body.distancia_1;
+  const distancia_2 = req.body.distancia_2;
+  const distancia_3 = req.body.distancia_3;
   
-  console.log('Distancia recibida:', humedad, temperatura);
+  console.log('Distancia recibida:', humedad, temperatura, distancia_1, distancia_2, distancia_3);
 
   // Guardar los datos en el array
-  datosESP32.push({
-    temperatura,
-    humedad,
-    nivel_luz,
-    timestamp: new Date().toISOString() // Agregar una marca de tiempo
-  });
+  // Guardar los datos en el array con el cálculo del porcentaje
+datosESP32.push({
+  temperatura,
+  humedad,
+  distancia_1,
+  distancia_2,
+  distancia_3,
+  porcentaje_distancia_1: calcularPorcentaje(distancia_1),
+  porcentaje_distancia_2: calcularPorcentaje(distancia_2),
+  porcentaje_distancia_3: calcularPorcentaje(distancia_3),
+  timestamp: new Date().toISOString() // Agregar una marca de tiempo
+});
+
+// Función para calcular el porcentaje según la distancia y devolverlo como cadena de texto con el símbolo de %
+function calcularPorcentaje(distancia) {
+  let porcentaje;
+  if (distancia >= 10) {
+    porcentaje = '20%';
+  } else if (distancia >= 5) {
+    porcentaje = '100%';
+  } else {
+    porcentaje = ((distancia / 10) * 100).toFixed(2) + '%'; // Porcentaje relativo al valor máximo (10)
+  }
+  return porcentaje;
+}
+
   // Conexión al servidor AMQP y envío de datos
   amqp.connect('amqp://52.205.27.36/', function(error0, connection) {
     if (error0) {
@@ -39,9 +61,13 @@ app.post('/esp32', (req, res) => {
       const exchange = 'amq.topic';
       const routingKey = 'esp32.mqtt';
       const messageObject = {
-        name: 'encierro 4',
-        temperatura: temperatura,
-        humedad: humedad,
+        temperature :temperatura,
+        humidity:humedad,
+        food:calcularPorcentaje(distancia_1),
+        water:calcularPorcentaje(distancia_2),
+        enclosureId: 44,
+        datoId: 2,
+        date: new Date().toISOString() 
       };
       const msg = JSON.stringify(messageObject);
       channel.publish(exchange, routingKey, Buffer.from(msg));
